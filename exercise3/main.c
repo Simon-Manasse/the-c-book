32fe31c2df16be4e758ceafa51c9c1ecd0f81acc
Simon-Manasse <simo0047@hz.nl>
2025-02-27 03:04:41 +0100

    solved issues with the functions

--- exercise3/main.c
+++ exercise3/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define ARRAY_COUNT 10
 
 int give_back_integer(int ch[],int count){
-  int i;
+  int i, sum;
+  sum=0;
   char str[count];
   for (i=0;i<count;i++) {
-    str[i] = ch[i];
-    printf("str: %c; ch: %d\n",str[i], ch[i]);
+    sum += (ch[i] - 48) * (int)(pow(10,(count-i-1)));
   }
-  return atoi(str);
+  return sum;
 }
 
 int main(int argc, char *argv[])
---
+++ exercise4/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ARRAY_COUNT 10
+
+int give_back_integer(char chars[],int count){
+  int i, sum = 0;
+  for (i = 0; i < count; i++) {
+    sum = sum * 10 + (chars[i] - '0');
+  }
+  return sum;
+}
+
+
+void read_string_from_user(char chars[],int* count_ptr){
+  int ch, count = 0;
+  ch = getchar();
+
+  while (ch != '\n' && count < ARRAY_COUNT -1) {
+    chars[count] = (char) ch;
+    count++;
+    ch = getchar();
+  }
+  chars[count] = '\0';
+  *count_ptr = count;
+}
+
+// Todo: change count to ARRAY_COUNT
+
+int main(int argc, char *argv[])
+{
+  char chars[ARRAY_COUNT];
+  int inputed_numbers[ARRAY_COUNT];
+  int count_of_numbers = 0;
+  int i;
+
+  for (i = 0; i < ARRAY_COUNT; i++) {
+    read_string_from_user(chars,&count_of_numbers);
+    inputed_numbers[i] = give_back_integer(chars,count_of_numbers);
+  }
+  printf("inputed numbers in order:\n");
+
+  for (i=0;i<ARRAY_COUNT;i++) {
+    printf("%d\n",inputed_numbers[i]);
+  }
+  return EXIT_SUCCESS;
+}
