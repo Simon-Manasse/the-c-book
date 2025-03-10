da50db3e24cef733f03d6eb72a0a7c92a30a71e5
Simon-Manasse <simo0047@hz.nl>
2025-03-10 13:10:10 +0100

    finished exercise 5

--- exercise5/main.c
+++ exercise5/main.c
@@ -5,7 +5,70 @@
 
 #define ARRAY_COUNT 10
 
-
+char* parse_to_hex(int number){
+  char hex[sizeof(int)];
+  char* binaryPtr = malloc(sizeof(hex));
+  int i, modolus;
+  for (i=0;i<sizeof(hex);i++)hex[i] = '0';
+  i=sizeof(hex)-1;
+  while (number >=1) {
+    modolus = number%16;
+    switch (modolus) {
+      case 1:
+        hex[i] = '1';
+        break;
+      case 2:
+        hex[i] = '2';
+      break;
+      case 3:
+         hex[i] ='3';
+        break;
+      case 4:
+        hex[i] ='4';
+        break;
+      case 5:
+         hex[i] ='5';
+        break;
+      case 6:
+        hex[i] ='6';
+        break;
+      case 7:
+         hex[i] ='7';
+        break;
+      case 8:
+        hex[i] ='8';
+        break;
+      case 9:
+      hex[i] ='9';
+        break;
+      case 10:
+        hex[i] ='A';
+        break;
+      case 11:
+      hex[i] ='B';
+        break;
+      case 12:
+        hex[i] ='C';
+        break;
+      case 13:
+      hex[i] ='D';
+        break;
+      case 14:
+        hex[i] ='E';
+        break;
+      case 15:
+      hex[i] ='F';
+        break;
+      default:
+        hex[i]='0';
+    }
+  // todo: add logic for deviding the number with 16
+  number = number/16;
+  i--;
+  }
+  memcpy(binaryPtr,hex ,sizeof(hex)/sizeof(char));
+  return binaryPtr;
+}
 
 char* parse_to_binary(int number){
   char binary[sizeof(int)*4], tmp;
@@ -72,8 +135,11 @@ int main(int argc, char *argv[])
   }
   for (i=0;i < ARRAY_COUNT;i++) {
     char* binary = parse_to_binary(inputed_numbers[i]);
-    printf("In binary: %s\t In decimal: %d\n",binary,inputed_numbers[i]);
+    char* hex = parse_to_hex(inputed_numbers[i]);
+    printf("In binary: %s\t In decimal: %d\t In hex: %s\n",binary,inputed_numbers[i], hex);
+    free(hex);
     free(binary);
   }
+  parse_to_hex(32);
   return EXIT_SUCCESS;
 }
